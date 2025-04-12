22c8619b1773a302ae52cd6620fdd2fae8342f21
newvegas <olddwarf3@gmail.com>
2025-04-12 16:18:12 +0500

    2 коммит

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter the number of elements you wish to enter: ");
+
         while (true) {
             int i = scanner.nextInt();
         }
